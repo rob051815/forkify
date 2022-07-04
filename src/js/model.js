84603b904e3fedb1d9bf3a67b69de63b247bfc5b
rob051815 <rob05182015@gmail.com>
2022-07-04 09:32:36 -0300

    feat: add pagination to recipes preview list

--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,13 @@
 import 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 export const state = {
   recipe: {},
   search: {
     query: '',
+    page: 1,
     results: [],
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -50,4 +52,12 @@ export const loadSearchResults = async function (query) {
     console.log(`${err} 💥💥💥💥`);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage; //0
+  const end = page * state.search.resultsPerPage; //9 (slice element in the "end" position is not included)
+
+  return state.search.results.slice(start, end);
 };
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,5 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
+
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
   _errorMessage = 'No recipes found for your query! Please try again ;)';
