4bfef0a9ded85bd82ace614b749e0b1115b3b516
rob051815 <rob05182015@gmail.com>
2022-07-05 17:22:47 -0300

    feat: add View.update() method
    updates only changed text and element attributes

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -16,6 +16,36 @@ export default class View {
     this._parentElement.innerHTML = '';
   }
 
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+    console.log(newElements);
+    console.log(curElements);
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      console.log(curEl, newEl.isEqualNode(curEl));
+
+      // Updates changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Updates change ATTRIBUTES
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+    });
+  }
+
   renderSpinner() {
     const markup = `
     <div class="spinner">
